01a3f2af265689228ecceb4fe5f5e150310c40c7
christoffer.fjellstrom <christoffer.fjellstrom@qlok.se>
2023-12-29 21:06:35 +0100

    Add project files.

---
+++ AOC25/Machine.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC25
+{
+    internal class Machine
+    {
+        public Dictionary<string, List<string>> Connections { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, string> Disconnected { get; set; } = new Dictionary<string, string>();
+        public int Hash { get; set; }
+
+        public Machine(string[] lines)
+        {
+            // Populate base list
+            foreach (var line in lines)
+            {
+                var parts = line.Split(": ");
+                Connections[parts[0]] = parts[1].Split(" ").ToList();
+            }
+
+            // Populate reverse direction
+            var tmpKeys = new List<string>(Connections.Keys);
+            foreach (var con in tmpKeys)
+            {
+                foreach (var dst in Connections[con])
+                {
+                    if (!Connections.ContainsKey(dst))
+                        Connections[dst] = new List<string>();
+
+                    Connections[dst].Add(con);
+                }
+            }
+        }
+
+        private Machine(Dictionary<string, List<string>> connctions, Dictionary<string, string> disconnected, int hash)
+        {
+            Connections = connctions;
+            Hash = hash;
+        }
+
+        public Machine Copy()
+        {
+            var disCopy = new Dictionary<string, string>(Disconnected);
+            return new Machine(Connections, disCopy, Hash);
+        }
+
+        public void RemoveConnection(string c1, string c2)
+        {
+            Disconnected[c1] = c2;
+            Disconnected[c2] = c1;
+        }
+
+        public int CountConnected()
+        {
+            string start = Connections.Keys.First();
+            var visited = new HashSet<string>();
+            var frontier = new Stack<string>();
+            frontier.Push(start);
+            while (frontier.Count > 0)
+            {
+                var cur = frontier.Pop();
+                if (visited.Contains(cur))
+                    continue;
+                visited.Add(cur);
+                foreach (var conn in Connections[cur])
+                {
+                    if(visited.Contains(conn))
+                        continue;
+                    if (Disconnected.ContainsKey(cur) && Disconnected[cur] ==  conn)
+                        continue;
+                    frontier.Push(conn);
+                }
+            }
+            return visited.Count();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Hash == Hash;
+        }
+
+        public override int GetHashCode()
+        {
+            return Hash;
+        }
+    }
+}
---
+++ AOC25/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace AOC25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int picksCount = 100;
+            const int topCount = 10;
+
+            var lines = File.ReadAllLines("input.txt");
+
+            var machine = new Machine(lines);
+
+            var total = machine.CountConnected();
+            Console.WriteLine($"Total nodes: {total}");
+
+            var r = new Random();
+            var picks = new HashSet<Tuple<string, string>>();
+            var allNodes = machine.Connections.Keys.ToList();
+            for (int i = 0; i < picksCount; i++)
+            {
+                var first = allNodes[r.Next(allNodes.Count())];
+                var second = allNodes[r.Next(allNodes.Count())];
+                if(first == second || picks.Contains(new (second, first)))
+                    continue;
+                picks.Add(new (first, second));
+            }
+
+            Console.WriteLine($"Made {picks.Count()} path picks");
+
+            var seen = new Dictionary<Tuple<string, string>, int>();
+
+            foreach (var nodePair in picks)
+            {
+                var prev = new Dictionary<string, string>();
+                var visited = new HashSet<string>();
+                var frontier = new Queue<string>();
+                frontier.Enqueue(nodePair.Item1);
+                while(frontier.Count() > 0)
+                {
+                    var cur = frontier.Dequeue();
+                    if (cur == nodePair.Item2)
+                        break;
+                    if (visited.Contains(cur))
+                        continue;
+                    visited.Add(cur);
+                    foreach (var neighbor in machine.Connections[cur])
+                    {
+                        if(visited.Contains(neighbor))
+                            continue;
+
+                        if (!prev.ContainsKey(neighbor))
+                            prev[neighbor] = cur;
+
+                        frontier.Enqueue(neighbor);
+                    }
+                }
+                var node = nodePair.Item2;
+                while (prev.ContainsKey(node))
+                {
+                    var prevNode = prev[node];
+                    List<string> keyParts = new List<string> { node, prevNode };
+                    keyParts.Sort();
+                    var key = new Tuple<string, string>(keyParts[0], keyParts[1]);
+                    if (seen.ContainsKey(key))
+                        seen[key]++;
+                    else
+                        seen[key] = 1;
+
+                    node = prevNode;
+                }
+            }
+
+            var topConnectionsWithCounts = seen.OrderByDescending(kvp => kvp.Value).Take(topCount).ToArray();
+
+            Console.WriteLine("Top visited connections:");
+            foreach (var kvp in topConnectionsWithCounts)
+            {
+                Console.WriteLine($"\t{kvp.Key}: {kvp.Value}");
+            }
+
+            var topConnections = topConnectionsWithCounts.Select(kvp => kvp.Key).ToArray();
+
+            for (int ai = 0;ai < topConnections.Length - 2;ai++)
+            {
+                var a = topConnections[ai];
+                for (int bi = ai + 1; bi < topConnections.Length - 1; bi++)
+                {
+                    var b = topConnections[bi];
+                    for (int ci = bi + 1; ci < topConnections.Length; ci++)
+                    {
+                        var c = topConnections[ci];
+                        machine.RemoveConnection(a.Item1, a.Item2);
+                        machine.RemoveConnection(b.Item1, b.Item2);
+                        machine.RemoveConnection(c.Item1, c.Item2);
+                        var count = machine.CountConnected();
+                        if(count < total)
+                        {
+                            Console.WriteLine($"Found cut: {string.Join(',', machine.Disconnected.Where((_, i) => i % 2 == 0))}");
+                            Console.WriteLine($"Answer: {count} * {total - count} = {count * (total - count)}");
+                            return;
+                        }
+                        machine.Disconnected.Clear();
+                    }
+                }
+            }
+            Console.WriteLine("No answer found");
+        }
+
+    }
+}
